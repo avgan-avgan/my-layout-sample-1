633fb19d13fd00376715d8d86c9800818cac1587
avganjan <avganjan@gmail.com>
2021-09-06 04:52:36 +0400

    Added carousel to the project

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,10 @@
+import {useEffect, useReducer} from "react";
 import {Link} from "react-router-dom";
 import "./mystyles/content.css"
+
 import items from "./data/catalogItemlist.json"
-import {useEffect, useReducer} from "react";
+import images from "./data/various.json"
+
 
 const initState = data => data
 
@@ -24,9 +27,56 @@ const reduce = (state, action)=>{
         )
     }
 }
+
+let cont_Index = 0
+
+const carouselInitState = data => data.map((v, i)=>({...v, left: 0, index: 0}))
+
+const carouselReduce = (state, action) => {
+    // console.log(action.type)
+    // console.log(action.left)
+    // console.log(action.index)
+    if (action.type === "start") return state.map((v, i)=>({...v, left: -action.left + "px"}))
+    if (action.type === "backward") {
+        cont_Index++
+        if (cont_Index >= state.length){
+            cont_Index = state.length-1
+        }
+        return state
+    }
+    if (action.type === "forward") {
+        cont_Index--
+        if (cont_Index < 0){
+            cont_Index = 0
+        }
+        return state
+    }
+}
+
 const Content = ()=>{
 
     const [state, dispatch] = useReducer(reduce, items, initState)
+    const [carouselState, carouselDispatch] = useReducer(carouselReduce, images, carouselInitState)
+
+
+    useEffect(()=>{
+        let container = document.querySelector(".cont-imageContainer")
+        function pushCarousel(){
+            let left = container.clientWidth
+            console.log(cont_Index)
+            cont_Index++
+            if (cont_Index >= carouselState.length){
+                cont_Index = 0
+            }
+            if (cont_Index < 0){
+                cont_Index = carouselState.length - 1
+            }
+
+            carouselDispatch({type: "start", left: left * cont_Index, index: cont_Index})
+            setTimeout(pushCarousel, 4000)
+        }
+        pushCarousel()
+    },[])
 
     return(
         <div className="cont-main">
@@ -67,7 +117,27 @@ const Content = ()=>{
             </div>
 
             <div className="cont-contentContainer">
-
+                <div className="cont-subContainer">
+                    <span className="cont-leftArr" onClick={()=>carouselDispatch({type: "backward"})}>
+                            <i className="fas fa-chevron-circle-left"/>
+                        </span>
+                    <span className="cont-rightArr" onClick={()=>carouselDispatch({type: "forward"})}>
+                            <i className="fas fa-chevron-circle-right"/>
+                        </span>
+                    <div className="cont-carouselContainer" style={{left: carouselState[cont_Index].left}}>
+                        {carouselState.map(({url, caption}, i)=>
+                            <div className="cont-imageContainer"  key={i}>
+                                <img src={url} alt={caption}/>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className="cont-stockStatContainer">
+                    <div className="cont-stockInfo">
+                        <div className="cont-stockInfoHeader">ИНТЕРНЕТ МАГАЗИН САНТЕХНИКИ VIVON.RU</div>
+                        <div className="cont-stockInfoParagraph">Прямо сейчас у нас продается <strong>65 596</strong> товаров для ванных комнат.</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
